254238d999f1de8dfe5ce8f434f87c49e8e24e76
unknown <roberthbardales@hotmail.com>
2026-03-17 23:44:12 -0500

    corrigiendo error

--- applications/entrada/api_urls.py
+++ applications/entrada/api_urls.py
@@ -11,8 +11,8 @@ app_name = 'entrada_api'
 
 urlpatterns = [
     path('entries/', EntryListAPI.as_view()),
-    path('entries/<slug:slug>/', EntryDetailAPI.as_view()),
     path('entries/create/', EntryCreateAPI.as_view()),
+    path('entries/<slug:slug>/', EntryDetailAPI.as_view()),
     # path('entries/<int:entry_id>/comments/', CommentListCreateAPI.as_view()),
     # path('entries/<int:entry_id>/like/', ToggleLikeAPI.as_view()),
 ]
